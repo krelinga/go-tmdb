4ee54f189f29482d3be6daca35869451d16c11f1
Andy Kreling <krelinga@gmail.com>
2025-05-23 22:09:30 +0000

    first try at client interface & full implementation.

---
+++ client.go
@@ -0,0 +1,77 @@
+package tmdb
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// Parameters for GET requests to the TMDB API.  Used with the Client interface.
+type GetParams map[string]string
+
+type ErrorClientHttp struct {
+	StatusCode int
+}
+
+type Client interface {
+	// Fetches data from the TMDB API.
+	// The path should be the endpoint you want to access, e.g., "/movie/popular".
+	// The params are optional query parameters to include in the request.
+	// The out parameter is where the response will be unmarshalled into, usually something from the raw package.
+	// The method returns an error if the request fails or if unmarshalling fails.
+	//
+	// Note that most users should not call this method directly, but instead pass a Client into
+	// other methods in this package.
+	//
+	// Implementations of this method should handle parallel calls to Get().
+	Get(ctx context.Context, path string, params GetParams, out any) error
+}
+
+type internalClient struct {
+	apiKey string
+}
+
+func (c *internalClient) Get(ctx context.Context, path string, params GetParams, out any) error {
+	baseURL := "https://api.themoviedb.org/3"
+	u, err := url.Parse(baseURL)
+	if err != nil {
+		panic(fmt.Sprintf("bad base URL: %s", baseURL))
+	}
+	u.Path = strings.TrimRight(u.Path, "/") + "/" + strings.TrimLeft(path, "/")
+
+	q := u.Query()
+	for k, v := range params {
+		q.Set(k, v)
+	}
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", u.String(), nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+c.apiKey)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(out); err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewClient(apiKey string) Client {
+	return &internalClient{
+		apiKey: apiKey,
+	}
+}
