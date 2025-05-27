9be08544846756f99ddd7e9d0dd364b70369c0aa
Andy Kreling <krelinga@gmail.com>
2025-05-27 00:00:40 +0000

    introduce ProductionCompanyId.

--- companies.go
+++ companies.go
@@ -1,8 +1,10 @@
 package tmdb
 
+type ProductionCompanyId int
+
 type ProductionCompanyShort struct {
-	Id            int       `json:"id"`
-	LogoImage     LogoImage `json:"logo_path"`
-	Name          string    `json:"name"`
-	OriginCountry string    `json:"origin_country"`
+	Id            ProductionCompanyId `json:"id"`
+	LogoImage     LogoImage           `json:"logo_path"`
+	Name          string              `json:"name"`
+	OriginCountry string              `json:"origin_country"`
 }
