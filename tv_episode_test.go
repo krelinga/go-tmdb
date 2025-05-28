c662d1e7ae2829bf6845410bf702d2900d192b2f
Andy Kreling <krelinga@gmail.com>
2025-05-28 21:39:33 +0000

    test TvEpisodeId.

--- tv_episode_test.go
+++ tv_episode_test.go
@@ -22,6 +22,7 @@ func TestGetTvEpisode(t *testing.T) {
 	assert.Equal(t, Series, episode.TvSeriesId, "TvSeriesId should match")
 	assert.Equal(t, SeasonNumber, episode.TvSeasonNumber, "TvSeasonNumber should match")
 	assert.Equal(t, EpisodeNumber, episode.TvEpisodeNumber, "TvEpisodeNumber should match")
+	assert.Equal(t, tmdb.TvEpisodeId(63056), episode.TvEpisodeId, "TvEpisodeId should match")
 	assert.Equal(t, "standard", episode.EpisodeType, "EpisodeType should be 'standard'")
 	assert.Equal(t, "Winter Is Coming", episode.Name, "Episode name should match")
 	const expectedOverview = "Jon Arryn, the Hand of the King, is dead. King Robert Baratheon plans to ask his oldest friend, Eddard Stark, to take Jon's place. Across the sea, Viserys Targaryen plans to wed his sister to a nomadic warlord in exchange for an army."
