fbf5bc262c2fe97865a2ee4e29b8ad2e6c4958a7
Andy Kreling <krelinga@gmail.com>
2025-05-28 14:49:34 +0000

    formatting.

--- keywords.go
+++ keywords.go
@@ -3,6 +3,6 @@ package tmdb
 type KeywordId int
 
 type Keyword struct {
-	KeywordId KeywordId    `json:"id"`
-	Name      string `json:"name"`
+	KeywordId KeywordId `json:"id"`
+	Name      string    `json:"name"`
 }
