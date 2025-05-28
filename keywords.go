7d40e5a836c25c677489fdc2f6b07b67e4af2920
Andy Kreling <krelinga@gmail.com>
2025-05-28 14:48:40 +0000

    use a specific type for Keyword Ids.

--- keywords.go
+++ keywords.go
@@ -1,6 +1,8 @@
 package tmdb
 
+type KeywordId int
+
 type Keyword struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	KeywordId KeywordId    `json:"id"`
+	Name      string `json:"name"`
 }
