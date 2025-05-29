64dcbf73c829b043f2f20d597bd2034207892d5b
Andy Kreling <krelinga@gmail.com>
2025-05-29 14:56:10 +0000

    Split TvSeason into TvSeasonSum

--- tv_season.go
+++ tv_season.go
@@ -10,16 +10,22 @@ type TvSeasonId int
 type TvSeasonNumber int
 
 type TvSeason struct {
+	TvSeasonSum
 	// TODO: what is this?
-	UnderscoreId   string          `json:"_id"`
-	AirDate        DateYYYYMMDD    `json:"air_date"`
-	TvEpisodes     []*TvEpisodeSum `json:"episodes"`
-	Name           string          `json:"name"`
-	Overview       string          `json:"overview"`
-	TvSeasonId     TvSeasonId      `json:"id"`
-	PosterImage    PosterImage     `json:"poster_path"`
-	TvSeasonNumber TvSeasonNumber  `json:"season_number"`
-	VoteAverage    float64         `json:"vote_average"`
+	UnderscoreId string          `json:"_id"`
+	TvEpisodes   []*TvEpisodeSum `json:"episodes"`
+}
+
+type TvSeasonSum struct {
+	AirDate DateYYYYMMDD `json:"air_date"`
+	// This isn't always set in the JSON output, but it is trivial to calculate in cases where it is not set.
+	EpisodeCount   int            `json:"episode_count"`
+	TvSeasonId     TvSeasonId     `json:"id"`
+	Name           string         `json:"name"`
+	Overview       string         `json:"overview"`
+	PosterImage    PosterImage    `json:"poster_path"`
+	TvSeasonNumber TvSeasonNumber `json:"season_number"`
+	VoteAverage    float64        `json:"vote_average"`
 }
 
 func GetTvSeason(client Client, id TvSeasonId, number TvSeasonNumber, options ...GetTvSeasonOption) (*TvSeason, error) {
@@ -44,6 +50,7 @@ func GetTvSeason(client Client, id TvSeasonId, number TvSeasonNumber, options ..
 	if err := json.Unmarshal(data, r); err != nil {
 		return nil, err
 	}
+	r.EpisodeCount = len(r.TvEpisodes)
 	return r, nil
 }
 
