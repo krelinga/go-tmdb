d1288f99cb53600ab82e23d3aad0135dff788991
Andy Kreling <krelinga@gmail.com>
2025-05-29 12:59:25 +0000

    first implementation of TvSeriesSearch().

--- tv_series_search.go
+++ tv_series_search.go
@@ -1,6 +1,11 @@
 package tmdb
 
-import "iter"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"iter"
+)
 
 type TvSeriesSearchResult struct {
 	TvSeriesSum
@@ -8,7 +13,49 @@ type TvSeriesSearchResult struct {
 }
 
 func SearchTvSeries(client Client, query string, options ...SearchTvSeriesOption) iter.Seq2[*TvSeriesSearchResult, error] {
-	return nil
+	return func(yield func(*TvSeriesSearchResult, error) bool) {
+		o := searchTvSeriesOptions{}
+		for _, opt := range options {
+			opt.applyToSearchTvSeriesOptions(&o)
+		}
+
+		type rawReply struct {
+			Page         int                   `json:"page"`
+			Results      []*TvSeriesSearchResult `json:"results"`
+			TotalPages   int                   `json:"total_pages"`
+			TotalResults int                   `json:"total_results"`
+		}
+		ctx := context.Background()
+		if o.useContext != nil {
+			ctx = *o.useContext
+		}
+		for page := 1; ; page++ {
+			params := GetParams{
+				"query":         query,
+			}
+			data, err := checkCode(client.Get(ctx, "/search/tv", params))
+			if err != nil {
+				yield(nil, err)
+				return
+			}
+			if o.rawReply != nil {
+				*o.rawReply = data
+			}
+			r := &rawReply{}
+			if err := json.Unmarshal(data, r); err != nil {
+				yield(nil, fmt.Errorf("unmarshalling search result: %w", err))
+				return
+			}
+			for _, tvSeries := range r.Results {
+				if !yield(tvSeries, nil) {
+					return
+				}
+			}
+			if r.Page >= r.TotalPages {
+				break
+			}
+		}
+	}
 }
 
 type SearchTvSeriesOption interface {
