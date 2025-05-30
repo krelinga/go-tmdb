471369244eaa9d7bf557d2658570b06abd35c38e
Andy Kreling <krelinga@gmail.com>
2025-05-30 22:54:01 +0000

    Test TvSeasons.

--- tv_series_test.go
+++ tv_series_test.go
@@ -131,4 +131,30 @@ func TestGetTvSeries(t *testing.T) {
 		},
 	}
 	assert.Equal(t, expectedProductionCountries, tv.ProductionCountries, "Unexpected production countries")
+	expectedSeasons := []*tmdb.TvSeasonSum{
+		{
+			AirDate:        tmdb.DateYYYYMMDD("2010-12-05"),
+			EpisodeCount:   283,
+			TvSeasonId:     3627,
+			Name:           "Specials",
+			Overview:       "",
+			PosterImage:    tmdb.PosterImage("/aos6lC1JGYt6ZRL85lgstNsfSeY.jpg"),
+			TvSeasonNumber: 0,
+			VoteAverage:    0.0,
+		},
+		{
+			AirDate:        tmdb.DateYYYYMMDD("2011-04-17"),
+			EpisodeCount:   10,
+			TvSeasonId:     3624,
+			Name:           "Season 1",
+			Overview:       "Trouble is brewing in the Seven Kingdoms of Westeros. For the driven inhabitants of this visionary world, control of Westeros' Iron Throne holds the lure of great power. But in a land where the seasons can last a lifetime, winter is coming...and beyond the Great Wall that protects them, an ancient evil has returned. In Season One, the story centers on three primary areas: the Stark and the Lannister families, whose designs on controlling the throne threaten a tenuous peace; the dragon princess Daenerys, heir to the former dynasty, who waits just over the Narrow Sea with her malevolent brother Viserys; and the Great Wall--a massive barrier of ice where a forgotten danger is stirring.",
+			PosterImage:    tmdb.PosterImage("/wgfKiqzuMrFIkU1M68DDDY8kGC1.jpg"),
+			TvSeasonNumber: 1,
+			VoteAverage:    8.4,
+		},
+	}
+	for _, es := range expectedSeasons {
+		assert.Contains(t, tv.TvSeasons, es, "TV seasons should contain expected season: %v", es)
+		checkPosterImage(t, es.PosterImage, config)
+	}
 }
