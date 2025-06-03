d5e840aae566a67d6af733c52a81249de5605f57
Andy Kreling <krelinga@gmail.com>
2025-06-03 03:58:28 +0000

    clean start

--- language.go
+++
@@ -1,9 +0,0 @@
-package tmdb
-
-type LanguageIso639_1 string
-
-type Language struct {
-	EnglishName string           `json:"english_name"`
-	Iso639_1    LanguageIso639_1 `json:"iso_639_1"`
-	Name        string           `json:"name"`
-}
