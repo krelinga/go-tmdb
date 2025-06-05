80202dfd9a24ec1506a4ddab2262d48bfa32f313
Andy Kreling <krelinga@gmail.com>
2025-06-05 05:45:59 +0000

    tweak comment.

--- new_ptr.go
+++ new_ptr.go
@@ -1,6 +1,6 @@
 package tmdb
 
-// This package makes extensive use of pointers to represent optional values.  This helper function provides a convenient way to create a pointer to a given value.
+// Returns a new pointer that points to the given value.
 func NewPtr[T any](v T) *T {
 	ptr := new(T)
 	*ptr = v
