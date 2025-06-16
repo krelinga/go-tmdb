556e4c84991cf86f73d0cd7c4f2aeb38d1f71b50
Andy Kreling <krelinga@gmail.com>
2025-06-16 04:58:55 +0000

    notes about how to move forward.

--- graph.go
+++ graph.go
@@ -38,3 +38,25 @@ func (g *Graph) init() {
 	g.Seasons = views.DictOfMap[SeasonKey, *Season]{M: g.seasons}
 	g.TvShows = views.DictOfMap[TvKey, *Tv]{M: g.tvShows}
 }
+
+// I've thought a lot about this and here's how this is going to go:
+// For each type of entity there will be a "data" struct that holds the keys along with a lot of optional fields for random data.
+// - This has all exported fields and no linkage to other types of entities.
+// - This also has a Update() method that will take a pointer to another "data" struct and update the fields of this one.
+// There will also be a corresponding "node" struct that provides all the linkage pointers.
+// - this will be all methods, no exported fields. (or maybe this will embed the "data" struct?)
+// - one getter method for each type of link that returns a bag view.
+// - one Add() method for each type of edge, which goes and updates the node at the other end of the edge as well.
+// Graph will have several families of methods to update the graph:
+// - Upsert() which will take a pointer to the "data" struct and return a pointer to the "node" struct.
+// - Rpc() which will use the API to fetch the data and return a pointer to the "node" struct.  Call Upsert() internally.
+// - Merge(), which will take pointers to other graph structures to be merged into this one.
+
+// Why not merge "data" and "node" structs?
+// - This is nicer for testing & creation syntax.  The "data" struct can be created with a simple literal, 
+//   and the "node" struct can be created with a few terse AddEdge() calls.
+
+// for keys, let's keep it simple:
+// - each "data" structure has a Key field that is of some type that uniquely identifies the entity.
+// - most "Key" fields will be backed by a typed string or int, but some will be backed by a struct named FooKey
+// - That way we can still require the nicer syntax around explicit type conversions, but we can avoid a bunch of single-field embedded structs.
