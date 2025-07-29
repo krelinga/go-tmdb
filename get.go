b266c254b5a27f4ec0c8d5b2460a567b9edf9332
Andy Kreling <krelinga@gmail.com>
2025-07-29 00:16:27 +0000

    add Try().

--- get.go
+++ get.go
@@ -11,3 +11,16 @@ func GetOr[T any](d Data[T], defaultValue T) T {
 	}
 	return val
 }
+
+// TODO: play around with this and decide if it's useful.
+func Try[T any](sharedErr *error, d Data[T]) T {
+	if *sharedErr != nil {
+		var zero T
+		return zero
+	}
+	val, err := d.get()
+	if err != nil {
+		*sharedErr = err
+	}
+	return val
+}
