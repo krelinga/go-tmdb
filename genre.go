3ef3f704503aebc48b2b3d3b19119e05a985dda5
Andy Kreling <krelinga@gmail.com>
2025-08-21 21:50:27 +0000

    Support GetMovieGenres() and GetTvGenres().

--- genre.go
+++ genre.go
@@ -1,6 +1,10 @@
 package tmdb
 
-import "github.com/krelinga/go-jsonflex"
+import (
+	"context"
+
+	"github.com/krelinga/go-jsonflex"
+)
 
 type Genre Object
 
@@ -10,4 +14,18 @@ func (g Genre) ID() (int32, error) {
 
 func (g Genre) Name() (string, error) {
 	return jsonflex.GetField(g, "name", jsonflex.AsString())
+}
+
+type Genres Object
+
+func (g Genres) Genres() ([]Genre, error) {
+	return jsonflex.GetField(g, "genres", jsonflex.AsArray(jsonflex.AsObject[Genre]()))
+}
+
+func GetMovieGenres(ctx context.Context, client Client, opts ...RequestOption) (Genres, error) {
+	return client.GetObject(ctx, "/3/genre/movie/list", opts...)
+}
+
+func GetTvGenres(ctx context.Context, client Client, opts ...RequestOption) (Genres, error) {
+	return client.GetObject(ctx, "/3/genre/tv/list", opts...)
 }
