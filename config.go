c3c4d8402bfa90aa3f38e106458c84cccc41ad6d
Andy Kreling <krelinga@gmail.com>
2025-08-21 20:41:04 +0000

    GetConfigDetails() and stub test.

---
+++ config.go
@@ -0,0 +1,51 @@
+package tmdb
+
+import (
+	"context"
+
+	"github.com/krelinga/go-jsonflex"
+)
+
+type ConfigDetails Object
+
+func (c ConfigDetails) Images() (ConfigImages, error) {
+	return jsonflex.GetField(c, "images", jsonflex.AsObject[ConfigImages]())
+}
+
+func (c ConfigDetails) ChangeKeys() ([]string, error) {
+	return jsonflex.GetField(c, "change_keys", jsonflex.AsArray(jsonflex.AsString()))
+}
+
+type ConfigImages Object
+
+func (c ConfigImages) BaseURL() (string, error) {
+	return jsonflex.GetField(c, "base_url", jsonflex.AsString())
+}
+
+func (c ConfigImages) SecureBaseURL() (string, error) {
+	return jsonflex.GetField(c, "secure_base_url", jsonflex.AsString())
+}
+
+func (c ConfigImages) BackdropSizes() ([]string, error) {
+	return jsonflex.GetField(c, "backdrop_sizes", jsonflex.AsArray(jsonflex.AsString()))
+}
+
+func (c ConfigImages) LogoSizes() ([]string, error) {
+	return jsonflex.GetField(c, "logo_sizes", jsonflex.AsArray(jsonflex.AsString()))
+}
+
+func (c ConfigImages) PosterSizes() ([]string, error) {
+	return jsonflex.GetField(c, "poster_sizes", jsonflex.AsArray(jsonflex.AsString()))
+}
+
+func (c ConfigImages) ProfileSizes() ([]string, error) {
+	return jsonflex.GetField(c, "profile_sizes", jsonflex.AsArray(jsonflex.AsString()))
+}
+
+func (c ConfigImages) StillSizes() ([]string, error) {
+	return jsonflex.GetField(c, "still_sizes", jsonflex.AsArray(jsonflex.AsString()))
+}
+
+func GetConfigDetails(ctx context.Context, client Client, opts ...RequestOption) (ConfigDetails, error) {
+	return client.Get(ctx, "/3/configuration", opts...)
+}
---
+++ config_test.go
@@ -0,0 +1,32 @@
+package tmdb_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/krelinga/go-tmdb"
+)
+
+func TestGetConfigDetails(t *testing.T) {
+	client := testClientOptions{useApiReadAccessToken: true}.newClient(t)
+	config, err := tmdb.GetConfigDetails(context.Background(), client)
+	if err != nil {
+		t.Fatalf("failed to get config details: %v", err)
+	}
+
+	checkChangeKey(t, config, "adult")
+}
+
+func checkChangeKey(t *testing.T, config tmdb.ConfigDetails, key string) {
+	t.Helper()
+	changeKeys, err := config.ChangeKeys()
+	if err != nil {
+		t.Fatalf("failed to get change keys: %v", err)
+	}
+	for _, k := range changeKeys {
+		if k == key {
+			return
+		}
+	}
+	t.Errorf("expected change key %q not found in %v", key, changeKeys)
+}
