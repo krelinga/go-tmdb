8369e5eef9f2f1062d8c1eaee8be9239dc4c17ab
Andy Kreling <krelinga@gmail.com>
2025-08-21 20:49:30 +0000

    test GetConfigDetails().

--- config_test.go
+++ config_test.go
@@ -16,6 +16,15 @@ func TestGetConfigDetails(t *testing.T) {
 	}
 
 	checkChangeKey(t, config, "adult")
+	checkChangeKey(t, config, "air_date")
+	checkChangeKey(t, config, "also_known_as")
+	checkField(t, "http://image.tmdb.org/t/p/", config, tmdb.ConfigDetails.Images, tmdb.ConfigImages.BaseURL)
+	checkField(t, "https://image.tmdb.org/t/p/", config, tmdb.ConfigDetails.Images, tmdb.ConfigImages.SecureBaseURL)
+	checkImageSize(t, config, tmdb.ConfigImages.BackdropSizes, "original")
+	checkImageSize(t, config, tmdb.ConfigImages.LogoSizes, "original")
+	checkImageSize(t, config, tmdb.ConfigImages.PosterSizes, "original")
+	checkImageSize(t, config, tmdb.ConfigImages.ProfileSizes, "original")
+	checkImageSize(t, config, tmdb.ConfigImages.StillSizes, "original")
 }
 
 func checkChangeKey(t *testing.T, config tmdb.ConfigDetails, key string) {
@@ -28,4 +37,15 @@ func checkChangeKey(t *testing.T, config tmdb.ConfigDetails, key string) {
 		return
 	}
 	t.Errorf("expected change key %q not found in %v", key, changeKeys)
+}
+
+func checkImageSize(t *testing.T, config tmdb.ConfigDetails, sizeType func(tmdb.ConfigImages) ([]string, error), wantSize string) {
+	t.Helper()
+	if images, err := config.Images(); err != nil {
+		t.Fatalf("failed to get images: %v", err)
+	} else if sizes, err := sizeType(images); err != nil {
+		t.Fatalf("failed to get image sizes: %v", err)
+	} else if !slices.Contains(sizes, wantSize) {
+		t.Errorf("expected size %q not found in %v", wantSize, sizes)
+	}
 }
