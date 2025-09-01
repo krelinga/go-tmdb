3f0b819d8786e5e0e1554633d4c5d85d7756b1e5
Andy Kreling <krelinga@gmail.com>
2025-09-01 04:59:01 +0000

    set up tmdb client.

--- tool/main.go
+++ tool/main.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/krelinga/go-tmdb"
 )
 
-type command func([]string) error
+type command func(tmdb.Client, []string) error
 
 var commands = map[string]command{
 	"hello": hello,
 }
 
-func hello(args []string) error {
+func hello(_ tmdb.Client, args []string) error {
 	fmt.Println("Hello, World!")
 	return nil
 }
@@ -23,13 +25,24 @@ func main() {
 		return
 	}
 
+	api_key := os.Getenv("TMDB_KEY")
+	read_access_token := os.Getenv("TMDB_READ_ACCESS_TOKEN")
+	if len(api_key) == 0 && len(read_access_token) == 0 {
+		log.Fatal("TMDB_KEY or TMDB_READ_ACCESS_TOKEN must be set")
+		return
+	}
+	client := tmdb.ClientOptions{
+		APIKey:          api_key,
+		APIReadAccessToken: read_access_token,
+	}.NewClient()
+
 	cmd, ok := commands[os.Args[1]]
 	if !ok {
 		log.Fatal("Unknown command:", os.Args[1])
 		return
 	}
 
-	if err := cmd(os.Args[2:]); err != nil {
+	if err := cmd(client, os.Args[2:]); err != nil {
 		log.Fatal("Error:", err)
 	}
 }
